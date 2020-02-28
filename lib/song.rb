3a9bc3f39b60c99dd1154335931de0d078211f68
Mitzi Estrada <m_estrada17@live.com>
2020-02-28 07:27:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,6 +31,15 @@ attr_accessor :name, :artist, :genre
   
   def self.genre_count
     genre_count = { }
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
   end
   
   
