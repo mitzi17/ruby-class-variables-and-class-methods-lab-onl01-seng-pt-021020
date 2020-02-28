9821fbafe8d26eff9163dfe4886014f1ae37b334
Mitzi Estrada <m_estrada17@live.com>
2020-02-28 07:19:42 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+  
+@@count = 0
+@@genres = [ ]
+@@artists = [ ]
+@@name_genre = { }
+
+  def initialize(name, artist, genre)
+    attr_accessor :name, :artist, :genre
+    @@count += 1
+    genre << @@genres
+    artist << @@artists
+    name << @@name_genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres = [ ]
+  end
+  
+  def self.artists
+    @@artists = [ ]
+  end
+  
+  def self.genre_count
+    name_genre { }
+  end
+  
+  
+  
+  
+    
+  
+  
+  
+  
+end
